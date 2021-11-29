c1e69917e405a54387c5b0df684207062433677a
Muhammed KOCAOĞLU <kocaoglu_muhammed@outlook.com>
2021-11-29 23:14:11 +0300

    Add files via upload

--- top_moving_average_filter.vhd
+++ top_moving_average_filter.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: Muhammed KOCOAĞLU
+-- Engineer: 
 -- 
 -- Create Date: 11/28/2021 09:49:05 PM
 -- Design Name: 
@@ -17,8 +17,10 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -29,79 +31,83 @@ USE IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-ENTITY top_moving_average_filter IS
-    PORT (
-        CLK       : IN STD_LOGIC;
-        data_i    : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        datavalid : IN STD_LOGIC;
-        data_o    : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        dataready : OUT STD_LOGIC
-    );
-END top_moving_average_filter;
+entity top_moving_average_filter is
+port (
+	CLK			: in std_logic;
+	data_i		: in std_logic_vector(31 downto 0);
+	datavalid	: in std_logic;
+	data_o		: out std_logic_vector(31 downto 0);
+	dataready	: out std_logic
+);
+end top_moving_average_filter;
 
-ARCHITECTURE Behavioral OF top_moving_average_filter IS
+architecture Behavioral of top_moving_average_filter is
 
-    COMPONENT design_1_wrapper IS
-        PORT (
-            ap_clk_0        : IN STD_LOGIC;
-            ap_ctrl_0_done  : OUT STD_LOGIC;
-            ap_ctrl_0_idle  : OUT STD_LOGIC;
-            ap_ctrl_0_ready : OUT STD_LOGIC;
-            ap_ctrl_0_start : IN STD_LOGIC;
-            ap_rst_0        : IN STD_LOGIC;
-            average_0       : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-            data_in_0       : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-            data_prev_0     : IN STD_LOGIC_VECTOR (31 DOWNTO 0)
+    component design_1_wrapper is
+        port (
+          ap_clk_0 : in STD_LOGIC;
+          ap_ctrl_0_done : out STD_LOGIC;
+          ap_ctrl_0_idle : out STD_LOGIC;
+          ap_ctrl_0_ready : out STD_LOGIC;
+          ap_ctrl_0_start : in STD_LOGIC;
+          ap_rst_0 : in STD_LOGIC;
+          average_0 : out STD_LOGIC_VECTOR ( 31 downto 0 );
+          data_in_0 : in STD_LOGIC_VECTOR ( 31 downto 0 );
+          data_prev_0 : in STD_LOGIC_VECTOR ( 31 downto 0 )
         );
-    END COMPONENT;
-    SIGNAL average_0       : STD_LOGIC_VECTOR (31 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL data_in_0       : STD_LOGIC_VECTOR (31 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL data_prev_0     : STD_LOGIC_VECTOR (31 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL ap_ctrl_0_done  : STD_LOGIC                      := '0';
-    SIGNAL ap_ctrl_0_idle  : STD_LOGIC                      := '0';
-    SIGNAL ap_ctrl_0_start : STD_LOGIC                      := '0';
+      end component;
+signal average_0        : STD_LOGIC_VECTOR ( 31 downto 0 ) := (others => '0');    
+signal data_in_0        : STD_LOGIC_VECTOR ( 31 downto 0 ) := (others => '0');    
+signal data_prev_0      : STD_LOGIC_VECTOR ( 31 downto 0 ) := (others => '0');  
+signal ap_ctrl_0_done   : STD_LOGIC := '0';
+signal ap_ctrl_0_idle   : STD_LOGIC := '0';
+signal ap_ctrl_0_start  : STD_LOGIC := '0';  
 
-    TYPE states IS (
-        S_IDLE,
-        S_CALCULATE
-    );
-    SIGNAL state : states := S_IDLE;
-    TYPE myarraytype IS ARRAY (0 TO 9) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL data_array : myarraytype := (OTHERS => (OTHERS => '0'));
-BEGIN
-    P_MAIN : PROCESS (CLK)
-    BEGIN
-        IF rising_edge(CLK) THEN
-            dataready <= '0';
-            CASE state IS
-                WHEN S_IDLE =>
-                    IF datavalid = '1' THEN
-                        state           <= S_CALCULATE;
-                        data_array      <= data_i & data_array(0 TO 8);
-                        ap_ctrl_0_start <= '1';
-                    END IF;
+type states is (
+	S_IDLE, 
+	S_CALCULATE
+);
+signal state : states := S_IDLE;
+	
+	
+type myarraytype is array (0 to 9) of std_logic_vector(31 downto 0);
+signal data_array : myarraytype := (others => (others => '0'));
+begin
+P_MAIN: process(CLK)
+begin
+if rising_edge(CLK) then
+	dataready	<= '0';
+	case state is 
+		when S_IDLE =>
+			if datavalid = '1' then
+				state	<= S_CALCULATE;
+				data_array	<= data_i & data_array(0 to 8);
+				ap_ctrl_0_start	<= '1';
+			end if;
+			
+		when S_CALCULATE =>
+			if ap_ctrl_0_done = '1' then
+				ap_ctrl_0_start	<= '0';
+				dataready		<= '1';
+				data_o		<= average_0;
+				state	<= S_IDLE;
+			end if;
+	end case;
+end if;
+end process;
 
-                WHEN S_CALCULATE =>
-                    IF ap_ctrl_0_done = '1' THEN
-                        ap_ctrl_0_start <= '0';
-                        dataready       <= '1';
-                        data_o          <= average_0;
-                        state           <= S_IDLE;
-                    END IF;
-            END CASE;
-        END IF;
-    END PROCESS;
-    design_1_wrapper_i : design_1_wrapper
-    PORT MAP(
-        ap_clk_0        => CLK,
-        ap_ctrl_0_done  => ap_ctrl_0_done,
-        ap_ctrl_0_idle  => ap_ctrl_0_idle,
-        ap_ctrl_0_ready => OPEN,
-        ap_ctrl_0_start => ap_ctrl_0_start,
-        ap_rst_0        => '0',
-        average_0       => average_0,
-        data_in_0       => data_array(0),
-        data_prev_0     => data_array(9)
-    );
 
-END Behavioral;
+    design_1_wrapper_i: design_1_wrapper 
+        port map (
+          ap_clk_0          => CLK,
+          ap_ctrl_0_done    => ap_ctrl_0_done,
+          ap_ctrl_0_idle    => ap_ctrl_0_idle,
+          ap_ctrl_0_ready   => open,
+          ap_ctrl_0_start   => ap_ctrl_0_start,
+          ap_rst_0          => '0',
+          average_0         => average_0,
+          data_in_0         => data_array(0),
+          data_prev_0       => data_array(9)
+        );
+
+end Behavioral;
